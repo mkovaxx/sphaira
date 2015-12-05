7da5c80f4e57799f69c329a23071f9c3a9210c57
Mate J Kovacs <mkovaxx@gmail.com>
2015-12-04 22:53:31 -0800

    move checks to constructor

--- src/main/cube_map.py
+++ src/main/cube_map.py
@@ -6,12 +6,12 @@ class CubeMap(object):
     @classmethod
     def from_array(self, array):
         faces = np.array([array] * 6)
+        return CubeMap(faces)
+
+    def __init__(self, faces):
         assert len(faces.shape) == 4
         (face_count, width, height, depth) = faces.shape
         assert face_count == 6
         assert width == height
         assert depth == 4
-        return CubeMap(faces)
-
-    def __init__(self, faces):
         self.faces = faces
