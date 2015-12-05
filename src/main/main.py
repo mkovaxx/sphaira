c0aad8f564d31dff7842880c2b443ad8a4de7b8d
Mate J Kovacs <mkovaxx@gmail.com>
2015-12-04 22:09:18 -0800

    remove dependence on gl in CubeMap

--- src/main/main.py
+++ src/main/main.py
@@ -17,8 +17,9 @@ class Sphaira(pyglet.window.Window):
         self.zoom = 2.5
         self.mesh = SphericalMesh(4)
         self.file_name = "resources/puppy.jpg"
+        # the reverse direction: image = Image.fromarray(array)
         self.cube_map = CubeMap.from_image(Image.open(self.file_name))
-        self.cube_map.send_to_gl()
+        self.send_cube_map_to_gl(self.cube_map)
 
     def update(self, dt):
         self.t += dt
@@ -80,6 +81,55 @@ class Sphaira(pyglet.window.Window):
         glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
         self.mesh.draw_triangles()
         glPopMatrix()
+
+    def send_cube_map_to_gl(self, cube_map):
+        # enable cube map texturing
+        glEnable(GL_TEXTURE_CUBE_MAP)
+        # generate and bind texture
+        self.texture_id = (GLuint * 1)()
+        glGenTextures(1, self.texture_id)
+        glBindTexture(GL_TEXTURE_CUBE_MAP, self.texture_id[0])
+        # set up texturing parameters
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_GENERATE_MIPMAP, GL_FALSE)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_BASE_LEVEL, 0)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_LEVEL, 0)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE)
+        # bind textures
+        cube_faces = (GLenum * 6)(
+            GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+            GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+            # pyglet reverses y axis
+            GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+            GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+            GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+        )
+        for (face_index, cube_face) in enumerate(cube_faces):
+            (face_count, width, height, depth) = cube_map.faces.shape
+            assert face_count == 6
+            data = cube_map.faces[face_index].ctypes.data
+            glTexImage2D(
+                cube_face, 0,
+                GL_RGB, width, height, 0,
+                GL_RGB, GL_UNSIGNED_BYTE, data
+            )
+        # set up texture coordinates
+        glEnable(GL_TEXTURE_GEN_S)
+        glEnable(GL_TEXTURE_GEN_T)
+        glEnable(GL_TEXTURE_GEN_R)
+        glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR)
+        glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR)
+        glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR)
+        x_axis = (GLfloat * 4)(1.0, 0.0, 0.0, 0.0)
+        y_axis = (GLfloat * 4)(0.0, 1.0, 0.0, 0.0)
+        z_axis = (GLfloat * 4)(0.0, 0.0, 1.0, 0.0)
+        glTexGenfv(GL_S, GL_OBJECT_PLANE, x_axis)
+        glTexGenfv(GL_T, GL_OBJECT_PLANE, y_axis)
+        glTexGenfv(GL_R, GL_OBJECT_PLANE, z_axis)
 
 
 def main():
