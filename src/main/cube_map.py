be5ec0608345127fd0ef40ea2788f6c9b3bbc3d9
Mate J Kovacs <mkovaxx@gmail.com>
2015-12-04 22:47:36 -0800

    check input array in CubeMap.from_array()

--- src/main/cube_map.py
+++ src/main/cube_map.py
@@ -6,6 +6,10 @@ class CubeMap(object):
     @classmethod
     def from_array(self, array):
         faces = np.array([array] * 6)
+        assert len(faces.shape) == 4
+        (face_count, width, height, depth) = faces.shape
+        assert face_count == 6
+        assert width == height
         return CubeMap(faces)
 
     def __init__(self, faces):
