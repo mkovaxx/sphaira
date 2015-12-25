c22995e6e2543ec3a50ec35a11b187c71084652f
Mate Kovacs <mkovaxx@gmail.com>
2015-12-25 13:48:49 -0800

    clean up dead code

--- src/cube_map.py
+++ src/cube_map.py
@@ -28,22 +28,9 @@ class CubeMap(object):
 
     @classmethod
     def from_sphere(cls, sphere):
-        projs = [
-            lambda t, u: Vector3([+1, -u, -t]), # +X
-            lambda t, u: Vector3([-1, -u, +t]), # -X
-            lambda t, u: Vector3([+t, +1, +u]), # +Y
-            lambda t, u: Vector3([+t, -1, -u]), # -Y
-            lambda t, u: Vector3([+t, -u, +1]), # +Z
-            lambda t, u: Vector3([-t, -u, -1]), # -Z
-        ]
         size = 1024
         faces = np.zeros((6, size, size, 4), dtype=np.float32)
         sphaira.cube_map_assign(faces, sphere.array, sphere.sampler)
-        # for (face, proj) in enumerate(projs):
-        #     for y in xrange(size):
-        #         for x in xrange(size):
-        #             v = proj(2.0*x/size - 1, 2.0*y/size - 1)
-        #             faces[face, y, x] = sphere.sample(v)
         return CubeMap(faces)
 
     def __init__(self, faces):
