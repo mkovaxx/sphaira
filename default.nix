6616b46ff6dc5bc1b4f06aaace7301aae505045f
Mate Kovacs <mkovaxx@gmail.com>
2016-02-04 09:13:36 -0800

    update default.nix

--- default.nix
+++ default.nix
@@ -1,20 +1,12 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-{ stdenv ? pkgs.stdenv
-, python ? pkgs.python
-, pillow ? pkgs.pythonPackages.pillow
-, pyglet ? pkgs.pythonPackages.pyglet
-, pyrr ? pkgs.pythonPackages.pyrr
-}:
-
-stdenv.mkDerivation {
-  name = "python-nix";
-  version = "0.1.0.0";
-  src = ./.;
-  buildInputs = [
-    python
-    pyglet
-    pyrr
-  ];
+with import <nixpkgs> {}; {
+  sphairaEnv = stdenv.mkDerivation {
+    name = "sphaira-env";
+    buildInputs = [
+      pythonPackages.numpy
+      pythonPackages.pillow
+      pythonPackages.pyglet
+      pythonPackages.pyrr
+      pythonPackages.pyside
+    ];
+  };
 }
